89fe471aa019385a5a88c7bd977c17d30155377a
Larry Luk <larryluk526@gmail.com>
2017-11-25 23:52:50 -0500

    Initial Commit

---
+++ SnapJournal/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SnapJournal
+//
+//  Created by Larry Luk on 2017-11-25.
+//  Copyright © 2017 Larry Luk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
