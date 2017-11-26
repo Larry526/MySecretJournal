49ef301845d1c1055deb659bf7d3af6b7b5b5e5c
Larry Luk <larryluk526@gmail.com>
2017-11-26 17:32:41 -0500

    basic layout update

---
+++ SnapJournal/Controller/EditViewController.h
@@ -0,0 +1,13 @@
+//
+//  EditViewController.h
+//  SnapJournal
+//
+//  Created by Larry Luk on 2017-11-26.
+//  Copyright © 2017 Larry Luk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EditViewController : UIViewController
+
+@end
---
+++ SnapJournal/Controller/EditViewController.m
@@ -0,0 +1,34 @@
+//
+//  EditViewController.m
+//  SnapJournal
+//
+//  Created by Larry Luk on 2017-11-26.
+//  Copyright © 2017 Larry Luk. All rights reserved.
+//
+
+#import "EditViewController.h"
+
+@interface EditViewController ()
+
+@end
+
+@implementation EditViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)backButtonPressed:(UIButton *)sender {
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+
+
+
+@end
