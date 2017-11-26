6b7c2186856f00190bab3ffd88ded365dc5fe647
Larry Luk <larryluk526@gmail.com>
2017-11-26 16:32:04 -0500

    core data initial

---
+++ SnapJournal/Model/DateHandler.m
@@ -0,0 +1,22 @@
+//
+//  DateHandler.m
+//  SnapJournal
+//
+//  Created by Larry Luk on 2017-11-26.
+//  Copyright © 2017 Larry Luk. All rights reserved.
+//
+
+#import "DateHandler.h"
+
+@implementation DateHandler
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        
+    }
+    return self;
+}
+
+@end
