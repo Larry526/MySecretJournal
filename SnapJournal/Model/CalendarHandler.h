ca4e7cf5f214f212ad09f785b18eb858eb9e6d26
brokeandfurious <forumbomb@gmail.com>
2017-11-27 11:34:27 -0500

    initial commit

---
+++ SnapJournal/Model/CalendarHandler.h
@@ -0,0 +1,13 @@
+//
+//  CalendarHandler.h
+//  SnapJournal
+//
+//  Created by Murat Ekrem Kolcalar on 11/27/17.
+//  Copyright © 2017 Larry Luk. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CalendarHandler : NSObject
+
+@end
