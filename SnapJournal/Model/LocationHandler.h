5ae8279c0a953366473593ff8d7c6f3353994b3a
Larry Luk <larryluk526@gmail.com>
2017-11-27 16:59:14 -0500

    initial commit core location

---
+++ SnapJournal/Model/LocationHandler.h
@@ -0,0 +1,13 @@
+//
+//  LocationHandler.h
+//  SnapJournal
+//
+//  Created by Larry Luk on 2017-11-27.
+//  Copyright © 2017 Larry Luk. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LocationHandler : NSObject
+
+@end
---
+++ SnapJournal/Model/LocationHandler.m
@@ -0,0 +1,13 @@
+//
+//  LocationHandler.m
+//  SnapJournal
+//
+//  Created by Larry Luk on 2017-11-27.
+//  Copyright © 2017 Larry Luk. All rights reserved.
+//
+
+#import "LocationHandler.h"
+
+@implementation LocationHandler
+
+@end
