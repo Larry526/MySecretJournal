410c9342589bee31703af6e596fb2ade20303479
Larry Luk <larryluk526@gmail.com>
2017-11-28 16:02:07 -0500

    initial commit

---
+++ SnapJournal/Model/WeatherAPI.h
@@ -0,0 +1,13 @@
+//
+//  WeatherAPI.h
+//  SnapJournal
+//
+//  Created by Larry Luk on 2017-11-28.
+//  Copyright © 2017 Larry Luk. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WeatherAPI : NSObject
+
+@end
---
+++ SnapJournal/Model/WeatherAPI.m
@@ -0,0 +1,13 @@
+//
+//  WeatherAPI.m
+//  SnapJournal
+//
+//  Created by Larry Luk on 2017-11-28.
+//  Copyright © 2017 Larry Luk. All rights reserved.
+//
+
+#import "WeatherAPI.h"
+
+@implementation WeatherAPI
+
+@end
